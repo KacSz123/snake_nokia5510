f554761e7a02d4724bcbe29cafbda0a95ee87ac5
Kacper Szczepanowski <kacperszcz159@gmail.com>
2023-10-03 10:01:15 +0200

    rough ver snake list

---
+++ snake.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct node
+{
+	bool headFlag;
+	int coords[2];
+	enum
+	{
+		LEFT = 20,
+		RIGHT = 30,
+		UP = 40,
+		DOWN = 60
+	} orientation;
+	struct node *next;
+	struct node *prev;
+
+} node_Typedef;
+
+typedef struct fruit
+{
+	int coords[2];
+} fruit_Typedef;
+
+void addSnakeNode(node_Typedef *h, int x, int y)
+{
+	node_Typedef *node_tmp = h;
+
+	while (node_tmp->next != NULL)
+	{
+		node_tmp = node_tmp->next;
+	}
+	node_tmp->next = (node_Typedef *)malloc(sizeof(node_Typedef));
+
+	node_tmp->next->coords[0] = x;
+	node_tmp->next->coords[1] = y;
+	node_tmp->next->orientation = node_tmp->orientation;
+
+	// node_tmp->next->headFlag = false;
+	node_tmp->next->prev = node_tmp;
+	node_tmp->next->next = NULL;
+}
+void snakeHeadInit(int x, int y, node_Typedef *h)
+{
+
+	h->coords[0] = x;
+	h->coords[1] = y;
+	h->orientation = UP;
+	// h->headFlag = true;
+	h->next = NULL;
+	h->prev = NULL;
+}
+
+void removeSnake(node_Typedef *h)
+{
+}
+
+void moveSnake(node_Typedef *h, bool ifEaten, int orient)
+{
+	node_Typedef *node_tmp = h;
+	int tmp[2];
+	tmp[0] = node_tmp->coords[0];
+	tmp[1] = node_tmp->coords[1];
+
+	while (node_tmp->next != NULL)
+	{
+		node_tmp = node_tmp->next;
+
+		tmp[0] = node_tmp->coords[0];
+		tmp[1] = node_tmp->coords[1];
+
+	}
+	while(node_tmp->prev!=NULL)
+	{
+		node_tmp->coords[0] = node_tmp->prev->coords[0];
+		node_tmp->coords[1] = node_tmp->prev->coords[1];
+		node_tmp->orientation = node_tmp->prev->orientation;
+		node_tmp = node_tmp->prev;
+	}
+	
+	if (ifEaten)
+	{
+		addSnakeNode(h, tmp[0], tmp[1]);
+
+	}
+	h->coords[0]+=1;
+	switch (orient)
+	{
+	case LEFT:
+		/* code */
+		break;
+	case RIGHT:
+		/* code */
+		break;
+	case UP:
+		/* code */
+		break;
+	case DOWN:
+		/* code */
+		break;
+	
+	default:
+		break;
+	}
+}
+
+
+int main(void)
+{
+
+
+	node_Typedef *head = (node_Typedef *)malloc(sizeof(node_Typedef));
+	snakeHeadInit(5, 5, head);
+
+	moveSnake(head, 1, UP);
+	moveSnake(head, 0, UP);
+	moveSnake(head, 1, UP);
+	moveSnake(head, 0, UP);
+	moveSnake(head, 1, UP);
+	moveSnake(head, 0, UP);
+	moveSnake(head, 1, UP);
+	moveSnake(head, 0, UP);
+	moveSnake(head, 1, UP);
+
+	return 0;
+}
