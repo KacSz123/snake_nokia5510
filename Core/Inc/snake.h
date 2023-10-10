602864c8cd1e5d69749236933f08c46eede8dcc6
Kacper Szczepanowski <kacperszcz159@gmail.com>
2023-10-10 17:52:00 +0200

    merge problem overcome

--- Core/Inc/snake.h
+++ Core/Inc/snake.h
@@ -10,4 +10,44 @@
 
 
 
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef	enum
+	orient{
+		LEFT = 20,
+		RIGHT = 30,
+		UP = 40,
+		DOWN = 60
+	} SnakeOrientation_Typedef;
+typedef struct node
+{
+	bool headFlag;
+	int coords[2];
+	SnakeOrientation_Typedef orientation;
+	struct node *next;
+	struct node *prev;
+
+} node_Typedef;
+
+typedef struct fruit
+{
+	int coords[2];
+} fruit_Typedef;
+
+
+void addSnakeNode(node_Typedef *h, int x, int y);
+
+void snakeHeadInit(int x, int y, node_Typedef *h);
+
+void removeSnake(node_Typedef *h);
+
+void moveSnake(node_Typedef *h, bool ifEaten, int orient);
+
+void changeOrientation(SnakeOrientation_Typedef orient);
+
+
+
+
 #endif /* INC_SNAKE_H_ */
